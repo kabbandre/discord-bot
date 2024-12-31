0948f1cee802dc57a3e12b660e021b68c9549e4b
Mihails Jeremejevs <kabbandre@gmail.com>
2024-12-30 20:44:02 -0600

    add-minecraft-ip to digital ocean firewall

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ import {
   InteractionResponseType,
   verifyKeyMiddleware,
 } from 'discord-interactions';
-import { getRandomEmoji } from './utils.js';
+import {getRandomEmoji} from './utils.js';
+import * as net from "node:net";
+import {createApiClient} from "dots-wrapper";
+import * as util from "node:util";
 
+const dots = createApiClient({token: process.env.DIGITAL_OCEAN_KEY});
 // Create an express app
 const app = express();
 // Get port, or default to 3000
@@ -18,13 +22,13 @@ const PORT = process.env.PORT || 3000;
  */
 app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async function (req, res) {
   // Interaction type and data
-  const { type, data } = req.body;
+  const {type, data} = req.body;
 
   /**
    * Handle verification requests
    */
   if (type === InteractionType.PING) {
-    return res.send({ type: InteractionResponseType.PONG });
+    return res.send({type: InteractionResponseType.PONG});
   }
 
   /**
@@ -32,7 +36,7 @@ app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async fun
    * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
    */
   if (type === InteractionType.APPLICATION_COMMAND) {
-    const { name } = data;
+    const {name} = data;
 
     // "test" command
     if (name === 'test') {
@@ -46,12 +50,76 @@ app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async fun
       });
     }
 
+    switch (name) {
+      case 'add-minecraft-ip':
+        try {
+          const {options: [{value: ipAddress}]} = data;
+          if (!net.isIPv4(ipAddress))
+            return res.send({
+              type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+              data: {
+                content: `IP Address \`${ipAddress}\` is invalid`,
+              },
+            });
+
+          const firewall = await dots.firewall.listFirewalls({per_page: 100}).then((firewalls) =>
+            firewalls.data.firewalls.find(({name}) => name === 'Minecraft-Pass'));
+
+          if (!firewall)
+            return res.send({
+              type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+              data: {
+                content: `Firewall was not found`,
+                mentions: [{username: 'Kabbandre'}]
+              },
+            });
+
+          if (firewall.inbound_rules.some(rule =>
+            rule.sources.addresses.includes(ipAddress)))
+            return res.send({
+              type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+              data: {
+                content: `\`${ipAddress}\` is already whitelisted`,
+              },
+            });
+
+          firewall.inbound_rules = firewall.inbound_rules.map(rule => {
+            rule.sources.addresses.push(ipAddress)
+            return rule
+          })
+
+          const {status} = await dots.firewall.updateFirewall(firewall);
+
+          if (status !== 200)
+            res.send({
+              type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+              data: {
+                content: `Updating firewall has crapped out, status: ${status}`,
+              },
+            });
+
+          return res.send({
+            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+            data: {
+              content: `Added \`${ipAddress}\`!`,
+            },
+          });
+        } catch (e) {
+          res.send({
+            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+            data: {
+              content: `Something went **TERRIBLY** wrong`,
+            },
+          });
+        }
+    }
+
     console.error(`unknown command: ${name}`);
-    return res.status(400).json({ error: 'unknown command' });
+    return res.status(400).json({error: 'unknown command'});
   }
 
   console.error('unknown interaction type', type);
-  return res.status(400).json({ error: 'unknown interaction type' });
+  return res.status(400).json({error: 'unknown interaction type'});
 });
 
 app.listen(PORT, () => {
--- commands.js
+++ commands.js
@@ -44,6 +44,22 @@ const CHALLENGE_COMMAND = {
   contexts: [0, 2],
 };
 
-const ALL_COMMANDS = [TEST_COMMAND, CHALLENGE_COMMAND];
+const ADD_MINECRAFT_IP_COMMAND = {
+  name: 'add-minecraft-ip',
+  description: 'Adds IP address to the Minecraft server',
+  options: [
+    {
+      type: 3,
+      name: 'ipaddress',
+      description: 'Enter your IP Address',
+      required: true
+    },
+  ],
+  type: 1,
+  integration_types: [0, 1],
+  contexts: [0, 2],
+}
+
+const ALL_COMMANDS = [TEST_COMMAND, CHALLENGE_COMMAND, ADD_MINECRAFT_IP_COMMAND];
 
 InstallGlobalCommands(process.env.APP_ID, ALL_COMMANDS);
