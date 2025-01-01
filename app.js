511d86a5a8bed7445829bbe1e349b11d61e40002
Mihails Jeremejevs <kabbandre@gmail.com>
2024-12-31 23:33:18 -0600

    added router

--- app.js
+++ app.js
@@ -15,12 +15,21 @@ const dots = createApiClient({token: process.env.DIGITAL_OCEAN_KEY});
 const app = express();
 // Get port, or default to 3000
 const PORT = process.env.PORT || 3000;
+const router = express.Router()
+
+router.get('/test', function (req, res) {
+  return res.send({
+    data: {
+      content: 'Hello world!'
+    }
+  })
+})
 
 /**
  * Interactions endpoint URL where Discord will send HTTP requests
  * Parse request body and verifies incoming requests using discord-interactions package
  */
-app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async function (req, res) {
+router.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async function (req, res) {
   // Interaction type and data
   const {type, data} = req.body;
 
@@ -121,6 +130,8 @@ app.post('/interactions', verifyKeyMiddleware(process.env.PUBLIC_KEY), async fun
   console.error('unknown interaction type', type);
   return res.status(400).json({error: 'unknown interaction type'});
 });
+
+app.use('/bot', router)
 
 app.listen(PORT, () => {
   console.log('Listening on port', PORT);
